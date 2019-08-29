a5e0d6ffb2c5215016797bc562f8b7d03dcdf0f5
3dud3 <jeremy.grafeuille2019@campus-eni.fr>
2019-08-29 15:27:15 +0200

    sortie en cours le 29aout

---
+++ src/Controller/afficherSortieController.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Controller;
+
+
+
+
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class afficherSortieController
+ * @package App\Controller
+ * @Route("/afficher", name="Sortie_afficher", methods={"GET", "POST"})
+ */
+class afficherSortieController extends Controller
+{
+
+    /**
+     * @param EntityManagerInterface $em
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/afficSortie", name="affich_affich", methods={"GET", "POST"})
+     */
+    public function afficherSortie(EntityManagerInterface $em){
+
+        // recuperation des sorties
+        $sorti = $em->getRepository('App:Sortie')->findAll();
+
+        // recuperation des sites
+        $site = $em->getRepository('App:Site')->findAll();
+
+
+
+
+
+
+        return $this->render('sortie/afficherSortie.html.twig', ['allSortie' => $sorti, 'allSite' => $site]);
+    }
+
+
+
+}
