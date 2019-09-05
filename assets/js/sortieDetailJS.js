7b92888d349dc03ced425c3fe71a73f82ee45985
ValentinBOSSARD <valentin.bossard2019@campus-eni.fr>
2019-09-05 09:35:01 +0200

    Création de la page user et bouton réactif

--- assets/js/sortieDetailJS.js
+++ assets/js/sortieDetailJS.js
@@ -56,3 +56,53 @@ $(document).ready(function () {
     }, 1000);
 
 })
+
+// fonction ajax inscription à une sortie
+$('.inscription').click(function(){
+//inscription offset-1 col-4 btn btn-success
+    $(this).removeClass('inscription btn btn-success').addClass('desincription btn btn-danger').text('Me désinscrire');
+
+    $.ajax({
+        url : '/afficher/inscription',
+        type: "POST",
+        data: {
+            sortieId:$(this).val()
+        },
+        success: function (data) {
+            console.log(data);
+            //alert("inscription réussi")
+            history.go()
+        },
+        error: function () {
+            alert("Erreur de JS");
+        }
+
+    })
+
+})
+
+// fonction ajax desinscription à une sortie
+$('.desincription').click(function(){
+    //desincription offset-1 col-5 btn btn-danger
+    $(this).removeClass('desincription btn btn-danger').addClass('inscription btn btn-success').text('M\'inscrire');
+
+    $.ajax({
+        url : '/afficher/desinscription',
+        type: "POST",
+        data: {
+            sortieId:$(this).val()
+        },
+        success: function (data) {
+            console.log(data);
+            //alert("desinscription réussi")
+            history.go()
+
+        },
+
+        error: function () {
+            alert("Erreur de JS");
+        }
+
+    })
+
+})
